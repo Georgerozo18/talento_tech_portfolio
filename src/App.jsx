6924935d60a52aee543a328ed9199bf18e7ec34d
Jorge Leonardo Hernandez Rozo <seergee18@gmail.com>
2024-07-23 01:34:30 -0500

    changing camera type

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ export const App = () => {
   useFrame((state, delta) => {
     cubeRef.current.rotation.y += delta
     // groupRef.current.rotation.y += delta
+    // const angle = state.clock.elapsedTime
+    // state.camera.position.x = Math.sin() * 8
+    // state.camera.position.z = Math.cos() * 8
+    // state.camera.lookAt(0, 0, 0)
   })
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,21 @@ import { App } from './App.jsx'
 import './main.css'
 import { Canvas } from '@react-three/fiber'
 
+const cameraSettings = {
+  fov: 45,
+  zoom: 100,
+  near: 0.01,
+  far: 200,
+  position: [9, 5, 12]
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Canvas>
+    <Canvas
+      dpr={1}
+      gl={{ antialias: true }}
+      // flat
+      orthographic camera={cameraSettings}>
       <App />
     </Canvas>
   </React.StrictMode>,
